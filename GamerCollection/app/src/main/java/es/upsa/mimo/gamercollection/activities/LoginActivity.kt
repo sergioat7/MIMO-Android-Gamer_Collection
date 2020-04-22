1e48317dbe6b2bd58b750f0dd8fb6e8b0135f5b5
Sergio Aragonés <sergioat7@gmail.com>
2020-04-22 16:18:12 +0200

    Created Login activity and Login view

---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package es.upsa.mimo.gamercollection.activities
+
+import android.os.Bundle
+import es.upsa.mimo.gamercollection.R
+
+class LoginActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
