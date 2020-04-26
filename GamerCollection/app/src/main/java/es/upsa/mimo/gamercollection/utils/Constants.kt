0266fc38566c5de6cbecb6cd563981a2609b1354
Sergio Aragonés <sergioat7@gmail.com>
2020-04-26 13:42:46 +0200

    Created Constants

---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/utils/Constants.kt
@@ -0,0 +1,11 @@
+package es.upsa.mimo.gamercollection.utils
+
+class Constants {
+    companion object {
+
+        const val preferencesName = "preferences"
+        const val newInstallationPrefName = "newInstallation"
+        const val userDataPrefName = "userData"
+        const val baseEndpoint = "https://videogames-collection-services.herokuapp.com/"
+    }
+}
