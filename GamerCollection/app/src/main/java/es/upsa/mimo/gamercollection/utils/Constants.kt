d03631d2bdd4d41932d986c92152820cb086a523
Sergio Aragonés <sergioat7@gmail.com>
2020-04-26 20:39:16 +0200

    Created AuthData class

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/utils/Constants.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/utils/Constants.kt
@@ -10,6 +10,7 @@ class Constants {
         const val preferencesName = "preferences"
         const val newInstallationPrefName = "newInstallation"
         const val userDataPrefName = "userData"
+        const val authDataPrefName = "authData"
         const val baseEndpoint = "https://videogames-collection-services.herokuapp.com/"
         const val acceptLanguageHeader = "Accept-Language"
 
