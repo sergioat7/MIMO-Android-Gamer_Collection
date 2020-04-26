e3e5c8398158432d8927497ac3354a74a812b2cc
Sergio Aragonés <sergioat7@gmail.com>
2020-04-26 20:44:33 +0200

    Created Login action

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/LoginFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/LoginFragment.kt
@@ -1,61 +1,105 @@
 package es.upsa.mimo.gamercollection.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import es.upsa.mimo.gamercollection.R
+import es.upsa.mimo.gamercollection.activities.MainActivity
+import es.upsa.mimo.gamercollection.models.AuthData
+import es.upsa.mimo.gamercollection.models.UserData
+import es.upsa.mimo.gamercollection.network.apiClient.LoginAPIClient
+import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import kotlinx.android.synthetic.main.fragment_login.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var sharedPrefHandler: SharedPreferencesHandler
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPrefHandler = SharedPreferencesHandler(context)
+        checkIsNewInstallation()
+        showMainView()
+    }
+
+    //MARK: - Private functions
+
+    private fun checkIsNewInstallation() {
+
+        if (sharedPrefHandler.isNewInstallation()) {
+            sharedPrefHandler.removeUserData()
+            sharedPrefHandler.removeCredentials()
+            sharedPrefHandler.setIsNewInstallation()
+        }
+    }
+
+    private fun showMainView() {
+
+        if (sharedPrefHandler.isLoggedIn()) {
+            val intent = Intent(context, MainActivity::class.java).apply {}
+            startActivity(intent)
+        } else {
+            initializeUI()
+        }
+    }
+
+    private fun initializeUI() {
+
+        val username = sharedPrefHandler.getUserData().username
+        editTextEmail.setText(username)
+
+        login_button.setOnClickListener {login()}
+        register_button.setOnClickListener {register()}
+    }
+
+    private fun login() {
+
+        val username = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+
+        //TODO show loading
+        LoginAPIClient.login(username, password, resources, { token ->
+
+            val userData = UserData(username, password, true)
+            val authData = AuthData(token)
+            sharedPrefHandler.storeUserData(userData)
+            sharedPrefHandler.storeCredentials(authData)
+            syncApp()
+        }, { errorResponse ->
+
+            //TODO hide loading
+            //TODO show error dialog
+            Toast.makeText(context, errorResponse.error, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun register() {
+        //TODO go to register view
+    }
+
+    private fun syncApp() {
+
+        //TODO get formats
+        //TODO get genres
+        //TODO get platforms
+        //TODO get states
+        //TODO get games
+        //TODO get sagas
+        val intent = Intent(context, MainActivity::class.java).apply {}
+        startActivity(intent)
+        //TODO hide loading
     }
 }
