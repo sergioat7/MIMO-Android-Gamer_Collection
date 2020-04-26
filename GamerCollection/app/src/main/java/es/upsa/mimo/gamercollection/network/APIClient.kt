ad101db7f4ec1b7f674132d4e0aaf17bebf9d8c4
Sergio Aragonés <sergioat7@gmail.com>
2020-04-26 18:42:25 +0200

    Modified APIClient to execute requests on background

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/APIClient.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/APIClient.kt
@@ -5,55 +5,68 @@ import com.google.gson.Gson
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.models.ErrorResponse
 import es.upsa.mimo.gamercollection.models.EmptyResponse
+import es.upsa.mimo.gamercollection.utils.Constants
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class APIClient {
+class APIClient(resources: Resources) {
     companion object {
 
         val gson = Gson()
 
-        inline fun <reified T, reified U> sendServer(request: Call<T>, success: (T) -> Unit, failure: (U) -> Unit) {
+        fun getRetrofit(): Retrofit {
+            return Retrofit.Builder().baseUrl(Constants.baseEndpoint).addConverterFactory(GsonConverterFactory.create()).build()
+        }
 
-            val call = request.execute()
+        inline fun <reified T, reified U> sendServer(resources: Resources, request: Call<T>, crossinline success: (T) -> Unit, crossinline failure: (U) -> Unit) {
 
-            val statusCode = call.code()
+            request.enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>?, response: Response<T>) {
 
-            if ( statusCode < 400 && (statusCode == 204 || T::class.java == EmptyResponse::javaClass) ) {
+                    if ( response.isSuccessful && (response.code() == 204 || T::class.java == EmptyResponse::javaClass) ) {
 
-                val objectData = gson.toJson("{}")
-                val arrayData = gson.toJson("[]")
+                        val objectData = gson.toJson("{}")
+                        val arrayData = gson.toJson("[]")
 
-                var response = gson.fromJson(objectData, T::class.java)
-                if (response != null) {
-                    success(response)
-                    return
+                        var result = gson.fromJson(objectData, T::class.java)
+                        if (result != null) {
+                            success(result)
+                            return
+                        }
+
+                        result = gson.fromJson(arrayData, T::class.java)
+                        if (result != null) {
+                            success(result)
+                            return
+                        }
+
+                        val error = ErrorResponse(Resources.getSystem().getString(R.string.ERROR_SERVER)) as U
+                        failure(error)
+                    } else if (response.isSuccessful && response.body() != null) {
+
+                        val result = response.body() as T
+                        success(result)
+                    } else if (!response.isSuccessful && response.errorBody() != null) {
+
+                        val errorResponse: U = gson.fromJson(
+                            response.errorBody()!!.charStream(), U::class.java
+                        )
+                        failure(errorResponse)
+                    } else {
+                        val error = ErrorResponse(Resources.getSystem().getString(R.string.ERROR_SERVER)) as U
+                        failure(error)
+                    }
                 }
 
-                response = gson.fromJson(arrayData, T::class.java)
-                if (response != null) {
-                    success(response)
-                    return
+                override fun onFailure(call: Call<T>?, t: Throwable) {
+
+                    val error = ErrorResponse(resources.getString(R.string.ERROR_SERVER)) as U
+                    failure(error)
                 }
-
-                val error = ErrorResponse(Resources.getSystem().getString(R.string.ERROR_SERVER)) as U
-                failure(error)
-                return
-            } else if ( statusCode < 400 &&  call.body() != null) {
-
-                val response = call.body() as T
-                success(response)
-            } else if (statusCode in 400..499 && call.errorBody() != null) {
-
-                val errorResponse: U = gson.fromJson(
-                    call.errorBody()!!.charStream(), U::class.java
-                )
-                failure(errorResponse)
-            } else {
-
-                val error = ErrorResponse(Resources.getSystem().getString(R.string.ERROR_SERVER)) as U
-                failure(error)
-                return
-            }
+            })
         }
     }
 }
