7c53cb57dd29307792f4ae60ac940ca351abb520
Sergio Aragonés <sergioat7@gmail.com>
2020-04-27 19:14:10 +0200

    Code refactoring

---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/base/BaseActivity.kt
@@ -0,0 +1,9 @@
+package es.upsa.mimo.gamercollection.activities.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+open class BaseActivity : AppCompatActivity() {
+
+}
