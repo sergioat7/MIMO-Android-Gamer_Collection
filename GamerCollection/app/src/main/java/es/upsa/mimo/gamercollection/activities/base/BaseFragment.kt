a66fde9aa3b2c0d0030211ac12c0373f0b65a88c
Sergio Aragonés <sergioat7@gmail.com>
2020-04-27 19:33:21 +0200

    Code refactoring

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/base/BaseFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/base/BaseFragment.kt
@@ -12,20 +12,20 @@ import es.upsa.mimo.gamercollection.R
 
 open class BaseFragment : Fragment() {
 
-    private var progressBar: ProgressBar? = null
+    private lateinit var progressBar: ProgressBar
 
     fun showLoading(view: View?) {
 
         if (view == null) return
         val context = view.context
         progressBar = ProgressBar(context)
-        progressBar!!.indeterminateDrawable.setColorFilter(ContextCompat.getColor(context, R.color.color3), android.graphics.PorterDuff.Mode.MULTIPLY)
+        progressBar.indeterminateDrawable.setColorFilter(ContextCompat.getColor(context, R.color.color3), android.graphics.PorterDuff.Mode.MULTIPLY)
         val parentLayout = LinearLayout(context)
         parentLayout.layoutParams = view.layoutParams
         parentLayout.gravity = Gravity.CENTER
         if (view.parent != null) (view.parent as ViewGroup).addView(parentLayout)
-        progressBar!!.isIndeterminate = true
-        progressBar!!.tag = view
+        progressBar.isIndeterminate = true
+        progressBar.tag = view
         parentLayout.addView(progressBar)
         view.visibility = View.GONE
     }
@@ -33,8 +33,8 @@ open class BaseFragment : Fragment() {
     fun hideLoading() {
 
         if (progressBar == null) return
-        val view = progressBar!!.tag as View
-        (view.parent as ViewGroup).removeView(progressBar!!.parent as View)
+        val view = progressBar.tag as View
+        (view.parent as ViewGroup).removeView(progressBar.parent as View)
         view.visibility = View.VISIBLE
     }
 }
