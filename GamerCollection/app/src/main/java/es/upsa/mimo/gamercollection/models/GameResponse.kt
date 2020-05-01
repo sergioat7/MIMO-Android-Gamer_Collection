845480aee671f221b364c5850d90bfc9e8da2341
Sergio Aragonés <sergioat7@gmail.com>
2020-05-02 00:10:46 +0200

    Inserted games in Room database

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/GameResponse.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/GameResponse.kt
@@ -1,48 +1,51 @@
 package es.upsa.mimo.gamercollection.models
 
+import androidx.room.*
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "Game")
 data class GameResponse(
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
     @SerializedName("name")
-    val name: String,
+    val name: String?,
     @SerializedName("platform")
-    val platform: String,
+    val platform: String?,
     @SerializedName("score")
     val score: Double,
     @SerializedName("pegi")
-    val pegi: String,
+    val pegi: String?,
     @SerializedName("distributor")
-    val distributor: String,
+    val distributor: String?,
     @SerializedName("developer")
-    val developer: String,
+    val developer: String?,
     @SerializedName("players")
-    val players: String,
+    val players: String?,
     @SerializedName("releaseDate")
-    val releaseDate: String,
+    val releaseDate: String?,
     @SerializedName("goty")
     val goty: Boolean,
     @SerializedName("format")
-    val format: String,
+    val format: String?,
     @SerializedName("genre")
-    val genre: String,
+    val genre: String?,
     @SerializedName("state")
-    val state: String,
+    val state: String?,
     @SerializedName("purchaseDate")
-    val purchaseDate: String,
+    val purchaseDate: String?,
     @SerializedName("purchaseLocation")
-    val purchaseLocation: String,
+    val purchaseLocation: String?,
     @SerializedName("price")
     val price: Double,
     @SerializedName("imageUrl")
-    val imageUrl: String,
+    val imageUrl: String?,
     @SerializedName("videoUrl")
-    val videoUrl: String,
+    val videoUrl: String?,
     @SerializedName("loanedTo")
-    val loanedTo: String,
+    val loanedTo: String?,
     @SerializedName("observations")
-    val observations: String,
+    val observations: String?,
     @SerializedName("saga")
     val saga: SagaResponse,
     @SerializedName("songs")
