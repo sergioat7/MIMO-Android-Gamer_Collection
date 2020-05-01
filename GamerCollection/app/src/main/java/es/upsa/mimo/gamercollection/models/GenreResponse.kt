10f2ce2e6ec15c1dc0c9e01e5ff2a0e743a6c758
Sergio Aragonés <sergioat7@gmail.com>
2020-05-02 00:23:22 +0200

    Code refactoring

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/GenreResponse.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/GenreResponse.kt
@@ -1,6 +1,5 @@
 package es.upsa.mimo.gamercollection.models
 
-import androidx.annotation.NonNull
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
