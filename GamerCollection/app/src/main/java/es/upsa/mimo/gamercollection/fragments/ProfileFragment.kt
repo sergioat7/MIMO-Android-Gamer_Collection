7f85eedad4c5ee8b88c0510e2032ac0cf581a2de
Sergio Aragonés <sergioat7@gmail.com>
2020-05-03 13:37:15 +0200

    Finished profile view design

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/ProfileFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/ProfileFragment.kt
@@ -1,15 +1,19 @@
 package es.upsa.mimo.gamercollection.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
+import es.upsa.mimo.gamercollection.network.apiClient.UserAPIClient
+import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : BaseFragment() {
+
+    private lateinit var sharedPrefHandler: SharedPreferencesHandler
+    private lateinit var userAPIClient: UserAPIClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +22,30 @@ class ProfileFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPrefHandler = SharedPreferencesHandler(context)
+        userAPIClient = UserAPIClient(resources)
+
+        initializeUI()
+    }
+
+    //MARK: - Private functions
+
+    private fun initializeUI() {
+
+        val userData = sharedPrefHandler.getUserData()
+        edit_text_user.setText(userData.username)
+        edit_text_password.setText(userData.password)
+
+        change_password_button.setOnClickListener {updatePassword()}
+        delete_user_button.setOnClickListener {deleteUser()}
+    }
+
+    private fun updatePassword() {
+    }
+
+    private fun deleteUser() {
+    }
 }
