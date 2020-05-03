689299ca547e7ee02412840ce065e09cdd28339b
Sergio Aragonés <sergioat7@gmail.com>
2020-05-03 14:20:10 +0200

    Created Put password request

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/apiService/UserAPIService.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/apiService/UserAPIService.kt
@@ -2,11 +2,9 @@ package es.upsa.mimo.gamercollection.network.apiService
 
 import es.upsa.mimo.gamercollection.models.LoginCredentials
 import es.upsa.mimo.gamercollection.models.LoginResponse
+import es.upsa.mimo.gamercollection.models.NewPassword
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.HeaderMap
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface UserAPIService {
 
@@ -21,4 +19,10 @@ interface UserAPIService {
     )
     @POST("users/register")
     fun register(@HeaderMap headers: Map<String, String>, @Body body: LoginCredentials): Call<Void>
+
+    @Headers(
+        "Content-Type:application/json"
+    )
+    @PUT("users/updatePassword")
+    fun updatePassword(@HeaderMap headers: Map<String, String>, @Body body: NewPassword): Call<Void>
 }
