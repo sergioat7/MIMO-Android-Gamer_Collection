ebf4c05b0032cdb810ef573f9f6823b054b27357
Sergio Aragonés <sergioat7@gmail.com>
2020-05-03 03:04:29 +0200

    Created Request action

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/RegisterFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/RegisterFragment.kt
@@ -1,21 +1,152 @@
 package es.upsa.mimo.gamercollection.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import es.upsa.mimo.gamercollection.R
+import es.upsa.mimo.gamercollection.activities.MainActivity
 import es.upsa.mimo.gamercollection.activities.base.BaseFragment
+import es.upsa.mimo.gamercollection.models.*
+import es.upsa.mimo.gamercollection.network.apiClient.*
+import es.upsa.mimo.gamercollection.persistence.repositories.*
+import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : BaseFragment() {
+
+    private lateinit var sharedPrefHandler: SharedPreferencesHandler
+    private lateinit var formatRepository: FormatRepository
+    private lateinit var genreRepository: GenreRepository
+    private lateinit var platformRepository: PlatformRepository
+    private lateinit var stateRepository: StateRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPrefHandler = SharedPreferencesHandler(context)
+        formatRepository = FormatRepository(requireContext())
+        genreRepository = GenreRepository(requireContext())
+        platformRepository = PlatformRepository(requireContext())
+        stateRepository = StateRepository(requireContext())
+
+        initializeUI()
+    }
+
+    //MARK: - Private functions
+
+    private fun initializeUI() {
+        register_button.setOnClickListener {register()}
+    }
+
+    private fun register() {
+
+        val username = editTextUser.text.toString()
+        val password = editTextPassword.text.toString()
+        val repeatPassword = editTextRepeatPassword.text.toString()
+
+        if (username.isEmpty() || password.isEmpty() || repeatPassword.isEmpty()) {
+            showPopupDialog(resources.getString(R.string.ERROR_REGISTRATION_EMPTY_DATA))
+            return
+        }
+
+        if (password != repeatPassword) {
+            showPopupDialog(resources.getString(R.string.ERROR_REGISTRATION_DIFFERENT_PASSWORDS))
+            return
+        }
+
+        showLoading(view)
+        UserAPIClient.register(username, password, resources, {
+            UserAPIClient.login(username, password, resources, { token ->
+
+                val userData = UserData(username, password, false)
+                val authData = AuthData(token)
+                sharedPrefHandler.run {
+                    storeUserData(userData)
+                    storeCredentials(authData)
+                }
+                syncApp(userData)
+            }, { errorResponse ->
+                manageError(errorResponse)
+            })
+        }, { errorResponse ->
+            manageError(errorResponse)
+        })
+    }
+
+    private fun syncApp(userData: UserData) {
+
+        FormatAPIClient.getFormats(resources, { formats ->
+            GenreAPIClient.getGenres(resources, { genres ->
+                PlatformAPIClient.getPlatforms(resources, { platforms ->
+                    StateAPIClient.getStates(resources, { states ->
+
+                        manageFormats(formats)
+                        manageGenres(genres)
+                        managePlatforms(platforms)
+                        manageStates(states)
+
+                        userData.isLoggedIn = true
+                        sharedPrefHandler.storeUserData(userData)
+                        goToMainView()
+                        hideLoading()
+                    }, { errorResponse ->
+                        manageError(errorResponse)
+                    })
+                }, { errorResponse ->
+                    manageError(errorResponse)
+                })
+            }, { errorResponse ->
+                manageError(errorResponse)
+            })
+        }, { errorResponse ->
+            manageError(errorResponse)
+        })
+    }
+
+    private fun goToMainView() {
+
+        val intent = Intent(context, MainActivity::class.java).apply {}
+        startActivity(intent)
+    }
+
+    private fun manageFormats(formats: List<FormatResponse>) {
+
+        for (format in formats) {
+            formatRepository.insertFormat(format)
+        }
+        formatRepository.removeDisableContent(formats)
+    }
+
+    private fun manageGenres(genres: List<GenreResponse>) {
+
+        for (genre in genres) {
+            genreRepository.insertGenre(genre)
+        }
+        genreRepository.removeDisableContent(genres)
+    }
+
+    private fun managePlatforms(platforms: List<PlatformResponse>) {
+
+        for (platform in platforms) {
+            platformRepository.insertPlatform(platform)
+        }
+        platformRepository.removeDisableContent(platforms)
+    }
+
+    private fun manageStates(states: List<StateResponse>) {
+
+        for (state in states) {
+            stateRepository.insertState(state)
+        }
+        stateRepository.removeDisableContent(states)
+    }
 }
