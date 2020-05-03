11ad26b582808e021d397d31085772404f2912fe
Sergio Aragonés <sergioat7@gmail.com>
2020-05-03 18:40:21 +0200

    Created toolbar menus

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/RankingFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/RankingFragment.kt
@@ -1,24 +1,54 @@
 package es.upsa.mimo.gamercollection.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import es.upsa.mimo.gamercollection.R
+import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
 
-/**
- * A simple [Fragment] subclass.
- */
-class RankingFragment : Fragment() {
+class RankingFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_ranking, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeUI()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.ranking_toolbar_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.action_synchronize -> {
+                openSyncPopup()
+                return true
+            }
+            R.id.action_filter -> {
+                filter()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    //MARK: - Private functions
+
+    private fun initializeUI() {
+    }
+
+    private fun filter(){
+    }
 }
