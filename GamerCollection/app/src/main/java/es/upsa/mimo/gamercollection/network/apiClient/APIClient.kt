d05be7513a7974858059ccee7807a26c97ff1ce7
Sergio Aragonés <sergioat7@gmail.com>
2020-05-12 17:51:18 +0200

    Changed requests timeout

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/apiClient/APIClient.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/apiClient/APIClient.kt
@@ -5,20 +5,33 @@ import com.google.gson.Gson
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.models.ErrorResponse
 import es.upsa.mimo.gamercollection.utils.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.Exception
+import java.util.concurrent.TimeUnit
+
 
 class APIClient {
     companion object {
 
         val gson = Gson()
 
+        private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build()
+
         fun getRetrofit(): Retrofit {
-            return Retrofit.Builder().baseUrl(Constants.baseEndpoint).addConverterFactory(GsonConverterFactory.create()).build()
+
+            return Retrofit.Builder()
+                .baseUrl(Constants.baseEndpoint)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         }
 
         inline fun <reified T, reified U> sendServer(resources: Resources, request: Call<T>, crossinline success: (T) -> Unit, crossinline failure: (U) -> Unit) {
