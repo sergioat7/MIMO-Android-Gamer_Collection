454b2c950a405244ef93e4285dc5575a089040dd
Sergio Aragonés <sergioat7@gmail.com>
2020-05-15 00:58:41 +0200

    Created Games list view

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/GamesFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/GamesFragment.kt
@@ -5,6 +5,7 @@ import android.view.*
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.activities.GameDetailActivity
 import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_games.*
 
 class GamesFragment : BaseFragment() {
 
@@ -50,14 +51,57 @@ class GamesFragment : BaseFragment() {
     //MARK: - Private functions
 
     private fun initializeUI() {
+
+        text_view_games_number.text = resources.getString(R.string.GAMES_NUMBER_TITLE, 0)
+
+        button_sort.setOnClickListener { sort() }
+
+        button_pending.text = resources.getString(R.string.GAMES_FILTER_BUTTON_TITLE_PENDING, 0)
+        button_pending.setOnClickListener {
+            it.isSelected = !it.isSelected
+            button_in_progress.isSelected = false
+            button_finished.isSelected = false
+            showPendingGames()
+        }
+        button_in_progress.text = resources.getString(R.string.GAMES_FILTER_BUTTON_TITLE_IN_PROGRESS, 0)
+        button_in_progress.setOnClickListener {
+
+            button_pending.isSelected = false
+            it.isSelected = !it.isSelected
+            button_finished.isSelected = false
+            showInProgressGames()
+        }
+        button_finished.text = resources.getString(R.string.GAMES_FILTER_BUTTON_TITLE_FINISHED, 0)
+        button_finished.setOnClickListener {
+
+            button_pending.isSelected = false
+            button_in_progress.isSelected = false
+            it.isSelected = !it.isSelected
+            showFinishedGames()
+        }
+    }
+
+    private fun filter() {
         //TODO
     }
 
-    private fun filter(){
-        //TODO
-    }
-
-    private fun add(){
+    private fun add() {
         launchActivity(GameDetailActivity::class.java)
+    }
+
+    private fun sort() {
+        //TODO
+    }
+
+    private fun showPendingGames() {
+        //TODO
+    }
+
+    private fun showInProgressGames() {
+        //TODO
+    }
+
+    private fun showFinishedGames() {
+        //TODO
     }
 }
