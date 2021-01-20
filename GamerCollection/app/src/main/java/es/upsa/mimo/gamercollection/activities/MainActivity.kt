c9cc77c360f4ffc725ac855628cbffbfb92001d3
Sergio Aragonés <sergioat7@gmail.com>
2021-01-20 22:46:09 +0100

    Removed settings view

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/MainActivity.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/activities/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_games, R.id.navigation_sagas, R.id.navigation_profile, R.id.navigation_settings
+                R.id.navigation_games, R.id.navigation_sagas, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
