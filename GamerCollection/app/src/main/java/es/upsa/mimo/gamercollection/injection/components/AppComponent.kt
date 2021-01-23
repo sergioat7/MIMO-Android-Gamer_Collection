ea28b402f87db88e9c3d2f2d7b0518ee502ee028
Sergio Aragonés <sergioat7@gmail.com>
2021-01-23 14:52:52 +0100

    Created LoginViewModel

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
@@ -8,6 +8,7 @@ import es.upsa.mimo.gamercollection.fragments.popups.PopupFilterDialogFragment
 import es.upsa.mimo.gamercollection.fragments.popups.PopupSyncAppDialogFragment
 import es.upsa.mimo.gamercollection.injection.modules.AppDatabaseModule
 import es.upsa.mimo.gamercollection.injection.modules.SharedPreferencesModule
+import es.upsa.mimo.gamercollection.viewmodelfactories.LoginViewModelFactory
 
 @Component(modules = [
     AppDatabaseModule::class,
@@ -15,13 +16,14 @@ import es.upsa.mimo.gamercollection.injection.modules.SharedPreferencesModule
 ])
 interface AppComponent {
 
+    fun inject(loginViewModelFactory: LoginViewModelFactory)
+
     fun inject(popupFilterDialogFragment: PopupFilterDialogFragment)
     fun inject(gameDetailFragment: GameDetailFragment)
     fun inject(gameDetailActivity: GameDetailActivity)
     fun inject(profileFragment: ProfileFragment)
     fun inject(gameSongsFragment: GameSongsFragment)
     fun inject(gamesFragment: GamesFragment)
-    fun inject(loginFragment: LoginFragment)
     fun inject(registerFragment: RegisterFragment)
     fun inject(sagaDetailFragment: SagaDetailFragment)
     fun inject(sagasFragment: SagasFragment)
