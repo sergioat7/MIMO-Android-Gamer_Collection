2238ceabe924d4d61336852222cb4c1138aff3ce
Sergio Aragonés <sergioat7@gmail.com>
2021-01-24 16:26:26 +0100

    Created SagasViewModel

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/SagasFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/SagasFragment.kt
@@ -2,35 +2,27 @@ package es.upsa.mimo.gamercollection.fragments
 
 import android.os.Bundle
 import android.view.*
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import es.upsa.mimo.gamercollection.BuildConfig
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.activities.GameDetailActivity
 import es.upsa.mimo.gamercollection.activities.SagaDetailActivity
 import es.upsa.mimo.gamercollection.adapters.SagasAdapter
 import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
-import es.upsa.mimo.gamercollection.injection.GamerCollectionApplication
+import es.upsa.mimo.gamercollection.models.SagaResponse
 import es.upsa.mimo.gamercollection.models.base.BaseModel
-import es.upsa.mimo.gamercollection.network.apiClient.SagaAPIClient
-import es.upsa.mimo.gamercollection.repositories.PlatformRepository
-import es.upsa.mimo.gamercollection.repositories.SagaRepository
-import es.upsa.mimo.gamercollection.repositories.StateRepository
-import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import es.upsa.mimo.gamercollection.viewmodelfactories.SagasViewModelFactory
+import es.upsa.mimo.gamercollection.viewmodels.SagasViewModel
 import kotlinx.android.synthetic.main.fragment_sagas.*
-import javax.inject.Inject
 
 class SagasFragment : BaseFragment(), SagasAdapter.OnItemClickListener {
 
-    @Inject
-    lateinit var sharedPrefHandler: SharedPreferencesHandler
-    @Inject
-    lateinit var sagaAPIClient: SagaAPIClient
-    @Inject
-    lateinit var sagaRepository: SagaRepository
-    @Inject
-    lateinit var platformRepository: PlatformRepository
-    @Inject
-    lateinit var stateRepository: StateRepository
+    //MARK: - Private properties
+
+    private lateinit var viewModel: SagasViewModel
+    private lateinit var sagasAdapter: SagasAdapter
+
+    // MARK: - Lifecycle methods
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,16 +34,12 @@ class SagasFragment : BaseFragment(), SagasAdapter.OnItemClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val application = activity?.application
-        (application as GamerCollectionApplication).appComponent.inject(this)
-
         initializeUI()
     }
 
     override fun onResume() {
         super.onResume()
-        getContent()
+        viewModel.getSagas()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -64,16 +52,20 @@ class SagasFragment : BaseFragment(), SagasAdapter.OnItemClickListener {
 
         when(item.itemId) {
             R.id.action_synchronize -> {
+
                 openSyncPopup()
                 return true
             }
             R.id.action_add -> {
-                add()
+
+                launchActivity(SagaDetailActivity::class.java)
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    //MARK: - Interface methods
 
     override fun onItemClick(sagaId: Int) {
 
@@ -87,63 +79,74 @@ class SagasFragment : BaseFragment(), SagasAdapter.OnItemClickListener {
         launchActivityWithExtras(GameDetailActivity::class.java, params)
     }
 
-    //MARK: - Private functions
+    //MARK: - Private methods
 
     private fun initializeUI() {
 
-        swipe_refresh_layout.isEnabled = sharedPrefHandler.getSwipeRefresh()
+        val application = activity?.application
+        viewModel = ViewModelProvider(this, SagasViewModelFactory(application)).get(SagasViewModel::class.java)
+        setupBindings()
+
+        swipe_refresh_layout.isEnabled = viewModel.swipeRefresh
         swipe_refresh_layout.setColorSchemeResources(R.color.color3)
         swipe_refresh_layout.setProgressBackgroundColorSchemeResource(R.color.color2)
         swipe_refresh_layout.setOnRefreshListener {
-            loadSagas()
+            viewModel.loadSagas()
         }
 
         recycler_view_sagas.layoutManager = LinearLayoutManager(requireContext())
-        val platforms = platformRepository.getPlatforms()
-        val states = stateRepository.getStates()
-        recycler_view_sagas.adapter = SagasAdapter(requireContext(), ArrayList(), ArrayList(), platforms, states, this)
+        sagasAdapter = SagasAdapter(
+            viewModel.sagas.value?.toMutableList() ?: mutableListOf(),
+            mutableListOf(),
+            viewModel.platforms,
+            viewModel.states,
+            requireContext(),
+            this
+        )
+        recycler_view_sagas.adapter = sagasAdapter
     }
 
-    private fun getContent() {
+    private fun setupBindings() {
 
-        val sagas = sagaRepository.getSagas().sortedBy { it.name }
+        viewModel.sagasLoading.observe(viewLifecycleOwner, { isLoading ->
 
-        val adapter = recycler_view_sagas.adapter
-        if (adapter != null && adapter is SagasAdapter) {
-            val items = ArrayList<BaseModel<Int>>()
-            val expandedIds = ArrayList<Int>()
-            for (saga in sagas) {
-                items.add(saga)
-                items.addAll(saga.games.sortedBy { it.releaseDate })
-                expandedIds.add(saga.id)
+            if (isLoading) {
+                showLoading()
+            } else {
+
+                swipe_refresh_layout.isRefreshing = false
+                hideLoading()
             }
-            adapter.items = items
-            adapter.expandedIds = expandedIds
-            adapter.notifyDataSetChanged()
+        })
+
+        viewModel.sagasError.observe(viewLifecycleOwner, { error ->
+
+            hideLoading()
+            manageError(error)
+        })
+
+        viewModel.sagas.observe(viewLifecycleOwner, {
+            showData(it)
+        })
+    }
+
+    private fun showData(sagas: List<SagaResponse>) {
+
+        sagasAdapter.resetList()
+
+        val items = mutableListOf<BaseModel<Int>>()
+        val expandedIds = mutableListOf<Int>()
+        for (saga in sagas) {
+
+            items.add(saga)
+            items.addAll(saga.games.sortedBy { it.releaseDate })
+            expandedIds.add(saga.id)
         }
+        sagasAdapter.setItems(items)
+        sagasAdapter.setExpandedIds(expandedIds)
+        sagasAdapter.notifyDataSetChanged()
+
         layout_empty_list.visibility = if (sagas.isNotEmpty()) View.GONE else View.VISIBLE
         swipe_refresh_layout.visibility = if (sagas.isNotEmpty()) View.VISIBLE else View.GONE
-    }
-
-    private fun add(){
-        launchActivity(SagaDetailActivity::class.java)
-    }
-
-    private fun loadSagas() {
-
-        sagaAPIClient.getSagas({
-
-
-            for (saga in it) {
-                sagaRepository.insertSaga(saga)
-            }
-            sagaRepository.removeDisableContent(it)
-            getContent()
-            swipe_refresh_layout.isRefreshing = false
-        },{
-
-            manageError(it)
-            swipe_refresh_layout.isRefreshing = false
-        })
     }
 }
---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewmodels/SagasViewModel.kt
@@ -0,0 +1,62 @@
+package es.upsa.mimo.gamercollection.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import es.upsa.mimo.gamercollection.models.ErrorResponse
+import es.upsa.mimo.gamercollection.models.PlatformResponse
+import es.upsa.mimo.gamercollection.models.SagaResponse
+import es.upsa.mimo.gamercollection.models.StateResponse
+import es.upsa.mimo.gamercollection.network.apiClient.SagaAPIClient
+import es.upsa.mimo.gamercollection.repositories.PlatformRepository
+import es.upsa.mimo.gamercollection.repositories.SagaRepository
+import es.upsa.mimo.gamercollection.repositories.StateRepository
+import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import javax.inject.Inject
+
+class SagasViewModel @Inject constructor(
+    sharedPreferencesHandler: SharedPreferencesHandler,
+    private val sagaAPIClient: SagaAPIClient,
+    platformRepository: PlatformRepository,
+    private val sagaRepository: SagaRepository,
+    stateRepository: StateRepository
+): ViewModel() {
+
+    //MARK: - Private properties
+
+    private val _sagasLoading = MutableLiveData<Boolean>()
+    private val _sagasError = MutableLiveData<ErrorResponse>()
+    private val _sagas = MutableLiveData<List<SagaResponse>>()
+
+    //MARK: - Public properties
+
+    val swipeRefresh: Boolean = sharedPreferencesHandler.getSwipeRefresh()
+    val platforms: List<PlatformResponse> = platformRepository.getPlatforms()
+    val states: List<StateResponse> = stateRepository.getStates()
+    val sagasLoading: LiveData<Boolean> = _sagasLoading
+    val sagasError: LiveData<ErrorResponse> = _sagasError
+    val sagas: LiveData<List<SagaResponse>> = _sagas
+
+    //MARK: - Public methods
+
+    fun getSagas() {
+        _sagas.value = sagaRepository.getSagas().sortedBy { it.name }
+    }
+
+    fun loadSagas() {
+
+        sagaAPIClient.getSagas({
+
+            for (saga in it) {
+                sagaRepository.insertSaga(saga)
+            }
+            sagaRepository.removeDisableContent(it)
+            _sagas.value = sagaRepository.getSagas()
+            _sagasLoading.value = false
+        },{
+
+            _sagasError.value = it
+            _sagasLoading.value = false
+        })
+    }
+}
