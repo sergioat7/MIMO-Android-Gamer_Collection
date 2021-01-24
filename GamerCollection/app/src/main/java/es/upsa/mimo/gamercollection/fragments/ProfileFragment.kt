3f0888af9bbeac498de25b5d6a7d138041a795b0
Sergio Aragonés <sergioat7@gmail.com>
2021-01-24 16:09:07 +0100

    Created ProfileViewModel

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/ProfileFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/ProfileFragment.kt
@@ -1,51 +1,27 @@
 package es.upsa.mimo.gamercollection.fragments
 
-import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.InputType
 import android.view.*
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.activities.LandingActivity
-import es.upsa.mimo.gamercollection.activities.LoginActivity
 import es.upsa.mimo.gamercollection.extensions.setReadOnly
 import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
-import es.upsa.mimo.gamercollection.injection.GamerCollectionApplication
-import es.upsa.mimo.gamercollection.models.AuthData
-import es.upsa.mimo.gamercollection.network.apiClient.*
-import es.upsa.mimo.gamercollection.repositories.*
 import es.upsa.mimo.gamercollection.utils.Constants
-import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import es.upsa.mimo.gamercollection.viewmodelfactories.ProfileViewModelFactory
+import es.upsa.mimo.gamercollection.viewmodels.ProfileViewModel
 import kotlinx.android.synthetic.main.fragment_profile.*
-import javax.inject.Inject
 
 class ProfileFragment : BaseFragment() {
 
-    @Inject
-    lateinit var sharedPrefHandler: SharedPreferencesHandler
-    @Inject
-    lateinit var formatAPIClient: FormatAPIClient
-    @Inject
-    lateinit var genreAPIClient: GenreAPIClient
-    @Inject
-    lateinit var platformAPIClient: PlatformAPIClient
-    @Inject
-    lateinit var stateAPIClient: StateAPIClient
-    @Inject
-    lateinit var userAPIClient: UserAPIClient
-    @Inject
-    lateinit var formatRepository: FormatRepository
-    @Inject
-    lateinit var gameRepository: GameRepository
-    @Inject
-    lateinit var genreRepository: GenreRepository
-    @Inject
-    lateinit var platformRepository: PlatformRepository
-    @Inject
-    lateinit var sagaRepository: SagaRepository
-    @Inject
-    lateinit var stateRepository: StateRepository
+    //MARK: - Private properties
+
+    private lateinit var viewModel: ProfileViewModel
+
+    // MARK: - Lifecycle methods
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,10 +33,6 @@ class ProfileFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val application = activity?.application
-        (application as GamerCollectionApplication).appComponent.inject(this)
-
         initializeUI()
     }
 
@@ -74,184 +46,91 @@ class ProfileFragment : BaseFragment() {
 
         when(item.itemId) {
             R.id.action_synchronize -> {
+
                 openSyncPopup()
                 return true
             }
             R.id.action_logout -> {
-                logout()
+
+                showPopupConfirmationDialog(resources.getString(R.string.profile_logout_confirmation)) {
+                    viewModel.logout()
+                }
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    //MARK: - Private functions
+    //MARK: - Private methods
 
     private fun initializeUI() {
 
-        val userData = sharedPrefHandler.getUserData()
-        edit_text_user.setText(userData.username)
-        edit_text_password.setText(userData.password)
+        val application = activity?.application
+        viewModel = ViewModelProvider(this, ProfileViewModelFactory(application)).get(ProfileViewModel::class.java)
+        setupBindings()
 
+        edit_text_user.setText(viewModel.userData.username)
         edit_text_user.setReadOnly(true, InputType.TYPE_NULL, 0)
+        edit_text_password.setText(viewModel.userData.password)
 
-        radio_button_en.isChecked = sharedPrefHandler.getLanguage() == "en"
-        radio_button_es.isChecked = sharedPrefHandler.getLanguage() == "es"
+        radio_button_en.isChecked = viewModel.language == "en"
+        radio_button_es.isChecked = viewModel.language == "es"
 
+        var position = 0
+        viewModel.sortingKey.let { sortingKey ->
+            position = resources.getStringArray(R.array.sorting_keys_ids).indexOf(sortingKey)
+        }
+        spinner_sorting_keys.setSelection(position)
         spinner_sorting_keys.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.color2))
         spinner_sorting_keys.adapter = Constants.getAdapter(
             requireContext(),
             resources.getStringArray(R.array.sorting_keys).toList(),
             true
         )
-        val sortingKeyIds = resources.getStringArray(R.array.sorting_keys_ids)
-        sortingKeyIds.firstOrNull { it == sharedPrefHandler.getSortingKey() }?.let {
-            spinner_sorting_keys.setSelection(sortingKeyIds.indexOf(it))
-        } ?: run {
-            spinner_sorting_keys.setSelection(0)
+
+        switch_swipe_refresh.isChecked = viewModel.swipeRefresh
+
+        button_change_password.setOnClickListener {
+
+            val language = if(radio_button_en.isChecked) "en" else "es"
+            val sortingKey = resources.getStringArray(R.array.sorting_keys_ids)[spinner_sorting_keys.selectedItemPosition]
+            viewModel.save(
+                edit_text_password.text.toString(),
+                language,
+                sortingKey,
+                switch_swipe_refresh.isChecked
+            )
         }
+        button_delete_user.setOnClickListener {
 
-        switch_swipe_refresh.isChecked = sharedPrefHandler.getSwipeRefresh()
-
-        button_change_password.setOnClickListener { updatePassword() }
-        button_delete_user.setOnClickListener { deleteUser() }
-    }
-
-    private fun logout() {
-
-        showPopupConfirmationDialog(resources.getString(R.string.profile_logout_confirmation)) {
-
-            showLoading()
-            userAPIClient.logout({
-
-                sharedPrefHandler.removePassword()
-                removeData()
-            }, {
-
-                sharedPrefHandler.removePassword()
-                removeData()
-            })
-        }
-    }
-
-    private fun updatePassword() {
-
-        val language = if(radio_button_en.isChecked) "en" else "es"
-        val sortingKey = resources.getStringArray(R.array.sorting_keys_ids)[spinner_sorting_keys.selectedItemPosition]
-        save(
-            edit_text_password.text.toString(),
-            language,
-            sortingKey,
-            switch_swipe_refresh.isChecked
-        )
-    }
-
-    private fun save (newPassword: String, newLanguage: String, newSortParam: String, newSwipeRefresh: Boolean) {
-
-        val changePassword = newPassword != sharedPrefHandler.getUserData().password
-        val changeLanguage = newLanguage != sharedPrefHandler.getLanguage()
-        val changeSortParam = newSortParam != sharedPrefHandler.getSortingKey()
-        val changeSwipeRefresh = newSwipeRefresh != sharedPrefHandler.getSwipeRefresh()
-
-        if (changePassword) {
-            showLoading()
-            userAPIClient.updatePassword(newPassword, {
-
-                sharedPrefHandler.storePassword(newPassword)
-                val userData = sharedPrefHandler.getUserData()
-                userAPIClient.login(userData.username, userData.password, {
-
-                    val authData = AuthData(it)
-                    sharedPrefHandler.storeCredentials(authData)
-                    hideLoading()
-                    if (changeLanguage) {
-                        reloadData()
-                    }
-                }, {
-                    manageError(it)
-                })
-            }, {
-                manageError(it)
-            })
-        }
-
-        if (changeSortParam) {
-            sharedPrefHandler.setSortingKey(newSortParam)
-        }
-
-        if (changeSwipeRefresh) {
-            sharedPrefHandler.setSwipeRefresh(newSwipeRefresh)
-        }
-
-        if (changeLanguage) {
-
-            sharedPrefHandler.setLanguage(newLanguage)
-            if (!changePassword) {
-                reloadData()
+            showPopupConfirmationDialog(resources.getString(R.string.profile_delete_confirmation)) {
+                viewModel.deleteUser()
             }
         }
     }
 
-    private fun reloadData() {
+    private fun setupBindings() {
 
-        showLoading()
+        viewModel.profileLoading.observe(viewLifecycleOwner, { isLoading ->
 
-        formatAPIClient.getFormats({ formats ->
-            genreAPIClient.getGenres({ genres ->
-                platformAPIClient.getPlatforms({ platforms ->
-                    stateAPIClient.getStates({ states ->
-
-                        formatRepository.manageFormats(formats)
-                        genreRepository.manageGenres(genres)
-                        platformRepository.managePlatforms(platforms)
-                        stateRepository.manageStates(states)
-
-                        val landing = Intent(requireContext(), LandingActivity::class.java)
-                        startActivity(landing)
-                        activity?.finish()
-                        hideLoading()
-                    }, {
-                        manageError(it)
-                    })
-                }, {
-                    manageError(it)
-                })
-            }, {
-                manageError(it)
-            })
-        }, {
-            manageError(it)
+            if (isLoading) {
+                showLoading()
+            } else {
+                hideLoading()
+            }
         })
-    }
 
-    private fun deleteUser() {
+        viewModel.profileError.observe(viewLifecycleOwner, { error ->
 
-        showPopupConfirmationDialog(resources.getString(R.string.profile_delete_confirmation)) {
+            if (error == null) {
 
-            showLoading()
-            userAPIClient.deleteUser({
+                launchActivity(LandingActivity::class.java)
+                activity?.finish()
+            } else {
 
-                sharedPrefHandler.removeUserData()
-                removeData()
-            }, {
-                manageError(it)
-            })
-        }
-    }
-
-    private fun removeData() {
-
-        sharedPrefHandler.removeCredentials()
-        val games = gameRepository.getGames()
-        for (game in games) {
-            gameRepository.deleteGame(game)
-        }
-        val sagas = sagaRepository.getSagas()
-        for (saga in sagas) {
-            sagaRepository.deleteSaga(saga)
-        }
-
-        hideLoading()
-        launchActivity(LoginActivity::class.java)
+                hideLoading()
+                manageError(error)
+            }
+        })
     }
 }
---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewmodels/ProfileViewModel.kt
@@ -0,0 +1,164 @@
+package es.upsa.mimo.gamercollection.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import es.upsa.mimo.gamercollection.models.AuthData
+import es.upsa.mimo.gamercollection.models.ErrorResponse
+import es.upsa.mimo.gamercollection.models.UserData
+import es.upsa.mimo.gamercollection.network.apiClient.*
+import es.upsa.mimo.gamercollection.repositories.*
+import es.upsa.mimo.gamercollection.utils.SharedPreferencesHandler
+import javax.inject.Inject
+
+class ProfileViewModel @Inject constructor(
+    private val sharedPreferencesHandler: SharedPreferencesHandler,
+    private val formatAPIClient: FormatAPIClient,
+    private val genreAPIClient: GenreAPIClient,
+    private val platformAPIClient: PlatformAPIClient,
+    private val stateAPIClient: StateAPIClient,
+    private val userAPIClient: UserAPIClient,
+    private val formatRepository: FormatRepository,
+    private val gameRepository: GameRepository,
+    private val genreRepository: GenreRepository,
+    private val platformRepository: PlatformRepository,
+    private val sagaRepository: SagaRepository,
+    private val stateRepository: StateRepository
+): ViewModel() {
+
+    //MARK: - Private properties
+
+    private val _profileLoading = MutableLiveData<Boolean>()
+    private val _profileError = MutableLiveData<ErrorResponse>()
+
+    //MARK: - Public properties
+
+    val userData: UserData = sharedPreferencesHandler.getUserData()
+    val language: String = sharedPreferencesHandler.getLanguage()
+    val sortingKey: String = sharedPreferencesHandler.getSortingKey()
+    val swipeRefresh: Boolean = sharedPreferencesHandler.getSwipeRefresh()
+    val profileLoading: LiveData<Boolean> = _profileLoading
+    val profileError: LiveData<ErrorResponse> = _profileError
+
+    //MARK: - Public methods
+
+    fun logout() {
+
+        _profileLoading.value = true
+        userAPIClient.logout({
+
+            sharedPreferencesHandler.removePassword()
+            resetDatabase()
+        }, {
+
+            sharedPreferencesHandler.removePassword()
+            resetDatabase()
+        })
+    }
+
+    fun save (newPassword: String, newLanguage: String, newSortParam: String, newSwipeRefresh: Boolean) {
+
+        val changePassword = newPassword != sharedPreferencesHandler.getUserData().password
+        val changeLanguage = newLanguage != sharedPreferencesHandler.getLanguage()
+        val changeSortParam = newSortParam != sharedPreferencesHandler.getSortingKey()
+        val changeSwipeRefresh = newSwipeRefresh != sharedPreferencesHandler.getSwipeRefresh()
+
+        if (changePassword) {
+            _profileLoading.value = true
+            userAPIClient.updatePassword(newPassword, {
+
+                sharedPreferencesHandler.storePassword(newPassword)
+                val userData = sharedPreferencesHandler.getUserData()
+                userAPIClient.login(userData.username, userData.password, {
+
+                    val authData = AuthData(it)
+                    sharedPreferencesHandler.storeCredentials(authData)
+                    _profileLoading.value = false
+                    if (changeLanguage) {
+                        reloadData()
+                    }
+                }, {
+                    _profileError.value = it
+                })
+            }, {
+                _profileError.value = it
+            })
+        }
+
+        if (changeSortParam) {
+            sharedPreferencesHandler.setSortingKey(newSortParam)
+        }
+
+        if (changeSwipeRefresh) {
+            sharedPreferencesHandler.setSwipeRefresh(newSwipeRefresh)
+        }
+
+        if (changeLanguage) {
+
+            sharedPreferencesHandler.setLanguage(newLanguage)
+            if (!changePassword) {
+                reloadData()
+            }
+        }
+    }
+
+    fun deleteUser() {
+
+        _profileLoading.value = true
+        userAPIClient.deleteUser({
+
+            sharedPreferencesHandler.removeUserData()
+            sharedPreferencesHandler.removeCredentials()
+            resetDatabase()
+        }, {
+            _profileError.value = it
+        })
+    }
+
+    //MARK: - Private methods
+
+    private fun reloadData() {
+
+        _profileLoading.value = true
+
+        formatAPIClient.getFormats({ formats ->
+            genreAPIClient.getGenres({ genres ->
+                platformAPIClient.getPlatforms({ platforms ->
+                    stateAPIClient.getStates({ states ->
+
+                        formatRepository.manageFormats(formats)
+                        genreRepository.manageGenres(genres)
+                        platformRepository.managePlatforms(platforms)
+                        stateRepository.manageStates(states)
+
+                        _profileLoading.value = false
+                        _profileError.value = null
+                    }, {
+                        _profileError.value = it
+                    })
+                }, {
+                    _profileError.value = it
+                })
+            }, {
+                _profileError.value = it
+            })
+        }, {
+            _profileError.value = it
+        })
+    }
+
+    private fun resetDatabase() {
+
+        val games = gameRepository.getGames()
+        for (game in games) {
+            gameRepository.deleteGame(game)
+        }
+        val sagas = sagaRepository.getSagas()
+        for (saga in sagas) {
+            sagaRepository.deleteSaga(saga)
+        }
+
+        _profileLoading.value = false
+        _profileError.value = null
+    }
+}
