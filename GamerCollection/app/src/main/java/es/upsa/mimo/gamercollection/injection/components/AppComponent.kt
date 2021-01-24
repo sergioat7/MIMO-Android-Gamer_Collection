7c54e456485a5bf0b76df6e44ac7f656a0561d6a
Sergio Aragonés <sergioat7@gmail.com>
2021-01-24 16:37:07 +0100

    Created PopupFilterViewModel

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
@@ -3,7 +3,6 @@ package es.upsa.mimo.gamercollection.injection.components
 import dagger.Component
 import es.upsa.mimo.gamercollection.activities.GameDetailActivity
 import es.upsa.mimo.gamercollection.fragments.*
-import es.upsa.mimo.gamercollection.fragments.popups.PopupFilterDialogFragment
 import es.upsa.mimo.gamercollection.injection.modules.AppDatabaseModule
 import es.upsa.mimo.gamercollection.injection.modules.SharedPreferencesModule
 import es.upsa.mimo.gamercollection.viewmodelfactories.*
@@ -16,15 +15,15 @@ interface AppComponent {
 
     fun inject(landingViewModelFactory: LandingViewModelFactory)
     fun inject(loginViewModelFactory: LoginViewModelFactory)
+    fun inject(popupFilterViewModelFactory: PopupFilterViewModelFactory)
     fun inject(popupSyncAppViewModelFactory: PopupSyncAppViewModelFactory)
     fun inject(profileViewModelFactory: ProfileViewModelFactory)
     fun inject(registerViewModelFactory: RegisterViewModelFactory)
+    fun inject(sagasViewModelFactory: SagasViewModelFactory)
 
-    fun inject(popupFilterDialogFragment: PopupFilterDialogFragment)
     fun inject(gameDetailFragment: GameDetailFragment)
     fun inject(gameDetailActivity: GameDetailActivity)
     fun inject(gameSongsFragment: GameSongsFragment)
     fun inject(gamesFragment: GamesFragment)
     fun inject(sagaDetailFragment: SagaDetailFragment)
-    fun inject(sagasFragment: SagasFragment)
 }
