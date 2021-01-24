4186e13d4237e921f86e1305d2ba11cc42fee4f7
Sergio Aragonés <sergioat7@gmail.com>
2021-01-24 16:14:29 +0100

    Created PopupSyncAppViewModel

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/injection/components/AppComponent.kt
@@ -2,15 +2,11 @@ package es.upsa.mimo.gamercollection.injection.components
 
 import dagger.Component
 import es.upsa.mimo.gamercollection.activities.GameDetailActivity
-import es.upsa.mimo.gamercollection.activities.LandingActivity
 import es.upsa.mimo.gamercollection.fragments.*
 import es.upsa.mimo.gamercollection.fragments.popups.PopupFilterDialogFragment
-import es.upsa.mimo.gamercollection.fragments.popups.PopupSyncAppDialogFragment
 import es.upsa.mimo.gamercollection.injection.modules.AppDatabaseModule
 import es.upsa.mimo.gamercollection.injection.modules.SharedPreferencesModule
-import es.upsa.mimo.gamercollection.viewmodelfactories.LandingViewModelFactory
-import es.upsa.mimo.gamercollection.viewmodelfactories.LoginViewModelFactory
-import es.upsa.mimo.gamercollection.viewmodelfactories.RegisterViewModelFactory
+import es.upsa.mimo.gamercollection.viewmodelfactories.*
 
 @Component(modules = [
     AppDatabaseModule::class,
@@ -20,15 +16,15 @@ interface AppComponent {
 
     fun inject(landingViewModelFactory: LandingViewModelFactory)
     fun inject(loginViewModelFactory: LoginViewModelFactory)
+    fun inject(popupSyncAppViewModelFactory: PopupSyncAppViewModelFactory)
+    fun inject(profileViewModelFactory: ProfileViewModelFactory)
     fun inject(registerViewModelFactory: RegisterViewModelFactory)
 
     fun inject(popupFilterDialogFragment: PopupFilterDialogFragment)
     fun inject(gameDetailFragment: GameDetailFragment)
     fun inject(gameDetailActivity: GameDetailActivity)
-    fun inject(profileFragment: ProfileFragment)
     fun inject(gameSongsFragment: GameSongsFragment)
     fun inject(gamesFragment: GamesFragment)
     fun inject(sagaDetailFragment: SagaDetailFragment)
     fun inject(sagasFragment: SagasFragment)
-    fun inject(popupSyncAppDialogFragment: PopupSyncAppDialogFragment)
 }
