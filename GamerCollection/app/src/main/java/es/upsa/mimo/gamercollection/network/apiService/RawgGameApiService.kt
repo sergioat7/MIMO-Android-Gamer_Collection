55f41bbbec3a1930a80ea775fb3d6eb1bb6a7b7f
Sergio Aragonés <sergioat7@gmail.com>
2021-03-11 20:30:42 +0100

    Integrated RAWG requests

---
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/apiService/RawgGameApiService.kt
@@ -0,0 +1,20 @@
+package es.upsa.mimo.gamercollection.network.apiService
+
+import es.upsa.mimo.gamercollection.models.rawg.RawgGameListResponse
+import es.upsa.mimo.gamercollection.models.rawg.RawgGameResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.QueryMap
+
+interface RawgGameApiService {
+
+    @Headers("User-Agent:GamerCollection")
+    @GET("games")
+    fun getGames(@QueryMap queryParams: Map<String, String>): Call<RawgGameListResponse>
+
+    @Headers("User-Agent:GamerDB")
+    @GET("games/{gameId}")
+    fun getGame(@Path(value = "gameId") gameId: Int, @QueryMap queryParams: Map<String, String>): Call<RawgGameResponse>
+}
