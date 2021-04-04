b49b6838f4d530c0a3e719dc2fb4b640d73b1d4f
Sergio Aragonés <sergioat7@gmail.com>
2021-04-04 02:25:41 +0200

    Included data binding in LoginFragment layout

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/LoginFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/fragments/LoginFragment.kt
@@ -1,36 +1,26 @@
 package es.upsa.mimo.gamercollection.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.activities.MainActivity
 import es.upsa.mimo.gamercollection.activities.RegisterActivity
+import es.upsa.mimo.gamercollection.databinding.FragmentLoginBinding
 import es.upsa.mimo.gamercollection.extensions.afterTextChanged
 import es.upsa.mimo.gamercollection.extensions.onFocusChange
-import es.upsa.mimo.gamercollection.fragments.base.BaseFragment
+import es.upsa.mimo.gamercollection.fragments.base.BindingFragment
 import es.upsa.mimo.gamercollection.utils.Constants
 import es.upsa.mimo.gamercollection.utils.Environment
 import es.upsa.mimo.gamercollection.viewmodelfactories.LoginViewModelFactory
 import es.upsa.mimo.gamercollection.viewmodels.LoginViewModel
-import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment : BaseFragment() {
+class LoginFragment : BindingFragment<FragmentLoginBinding>() {
 
     //MARK: - Private properties
 
     private lateinit var viewModel: LoginViewModel
 
     // MARK: - Lifecycle methods
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -50,41 +40,45 @@ class LoginFragment : BaseFragment() {
 
         val username = viewModel.username
         val user = if (username.isEmpty()) Environment.getUsername() else username
-        edit_text_user.setText(user)
         val password = if (username.isEmpty()) Environment.getPassword() else Constants.EMPTY_VALUE
-        edit_text_password.setText(password)
 
-        edit_text_user.afterTextChanged {
-            loginDataChanged()
-        }
-        edit_text_user.onFocusChange {
-            loginDataChanged()
-        }
+        with(binding) {
 
-        edit_text_password.afterTextChanged {
-            loginDataChanged()
-        }
-        edit_text_password.onFocusChange {
-            loginDataChanged()
-        }
+            editTextUser.setText(user)
+            editTextUser.afterTextChanged {
+                loginDataChanged()
+            }
+            editTextUser.onFocusChange {
+                loginDataChanged()
+            }
 
-        image_button_password.setOnClickListener {
-            Constants.showOrHidePassword(
-                edit_text_password,
-                image_button_password,
-                Constants.isDarkMode(context)
-            )
-        }
+            editTextPassword.setText(password)
+            editTextPassword.afterTextChanged {
+                loginDataChanged()
+            }
+            editTextPassword.onFocusChange {
+                loginDataChanged()
+            }
 
-        login_button.setOnClickListener {
+            imageButtonPassword.setOnClickListener {
+                Constants.showOrHidePassword(
+                    editTextPassword,
+                    imageButtonPassword,
+                    Constants.isDarkMode(context)
+                )
+            }
 
-            viewModel.login(
-                edit_text_user.text.toString(),
-                edit_text_password.text.toString()
-            )
-        }
-        register_button.setOnClickListener {
-            launchActivity(RegisterActivity::class.java)
+            loginButton.setOnClickListener {
+
+                viewModel.login(
+                    editTextUser.text.toString(),
+                    editTextPassword.text.toString()
+                )
+            }
+
+            registerButton.setOnClickListener {
+                launchActivity(RegisterActivity::class.java)
+            }
         }
     }
 
@@ -94,13 +88,13 @@ class LoginFragment : BaseFragment() {
 
             val loginState = it ?: return@observe
 
-            login_button.isEnabled = loginState.isDataValid
+            binding.loginButton.isEnabled = loginState.isDataValid
 
             if (loginState.usernameError != null) {
-                edit_text_user.error = getString(loginState.usernameError)
+                binding.editTextUser.error = getString(loginState.usernameError)
             }
             if (loginState.passwordError != null) {
-                edit_text_password.error = getString(loginState.passwordError)
+                binding.editTextPassword.error = getString(loginState.passwordError)
             }
         })
 
@@ -128,8 +122,8 @@ class LoginFragment : BaseFragment() {
     private fun loginDataChanged() {
 
         viewModel.loginDataChanged(
-            edit_text_user.text.toString(),
-            edit_text_password.text.toString()
+            binding.editTextUser.text.toString(),
+            binding.editTextPassword.text.toString()
         )
     }
 }
