d3f71d0a1fb5b94366abbb87e28266b90a310e2c
Sergio Aragonés <sergioat7@gmail.com>
2021-04-11 01:08:39 +0200

    Removed scope from SongRepository

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/repositories/SongRepository.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/repositories/SongRepository.kt
@@ -1,54 +1,40 @@
 package es.upsa.mimo.gamercollection.repositories
 
-import es.upsa.mimo.gamercollection.injection.modules.MainDispatcher
 import es.upsa.mimo.gamercollection.models.responses.ErrorResponse
 import es.upsa.mimo.gamercollection.models.responses.SongResponse
 import es.upsa.mimo.gamercollection.network.ApiManager
 import es.upsa.mimo.gamercollection.network.RequestResult
 import es.upsa.mimo.gamercollection.network.SongApiService
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class SongRepository @Inject constructor(
-    private val api: SongApiService,
-    @MainDispatcher private val mainDispatcher: CoroutineDispatcher
+    private val api: SongApiService
 ) {
 
-    //region Private properties
-    private val externalScope = CoroutineScope(Job() + mainDispatcher)
-    //endregion
-
     //region Public methods
-    fun createSong(
+    suspend fun createSong(
         gameId: Int,
         song: SongResponse,
         success: () -> Unit,
         failure: (ErrorResponse) -> Unit
     ) {
-        externalScope.launch {
+        when (val response = ApiManager.validateResponse(api.createSong(gameId, song))) {
 
-            when (val response = ApiManager.validateResponse(api.createSong(gameId, song))) {
-                is RequestResult.Success -> success()
-                is RequestResult.Failure -> failure(response.error)
-            }
+            is RequestResult.Success -> success()
+            is RequestResult.Failure -> failure(response.error)
         }
     }
 
-    fun deleteSong(
+    suspend fun deleteSong(
         gameId: Int,
         songId: Int,
         success: () -> Unit,
         failure: (ErrorResponse) -> Unit
     ) {
-        externalScope.launch {
+        when (val response = ApiManager.validateResponse(api.deleteSong(gameId, songId))) {
 
-            when (val response = ApiManager.validateResponse(api.deleteSong(gameId, songId))) {
-                is RequestResult.Success -> success()
-                is RequestResult.Failure -> failure(response.error)
-            }
+            is RequestResult.Success -> success()
+            is RequestResult.Failure -> failure(response.error)
         }
     }
     //endregion
