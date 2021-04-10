2db0d9158d1820d9e937367b5dac891769fc13e2
Sergio Aragonés <sergioat7@gmail.com>
2021-04-10 14:01:55 +0200

    Code refactoring

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/RequestResult.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/network/RequestResult.kt
@@ -4,6 +4,6 @@ import es.upsa.mimo.gamercollection.models.responses.ErrorResponse
 
 sealed class RequestResult<out T> {
     object Success : RequestResult<Nothing>()
-    data class JsonSuccess<out T>(val body: T?) : RequestResult<T>()
+    data class JsonSuccess<out T>(val body: T) : RequestResult<T>()
     data class Failure(val error: ErrorResponse) : RequestResult<Nothing>()
 }
