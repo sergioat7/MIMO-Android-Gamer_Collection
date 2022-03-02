77f1a7ffc581f3f1d36c7943c45f4273465d6d67
Sergio Aragones Tercero <sergio.aragones@almato.com>
2022-03-02 16:46:50 +0100

    Code refactoring

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewmodels/PopupFilterViewModel.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewmodels/PopupFilterViewModel.kt
@@ -1,11 +1,6 @@
 package es.upsa.mimo.gamercollection.viewmodels
 
-import android.content.Context
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.lifecycle.ViewModel
-import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.models.responses.FormatResponse
 import es.upsa.mimo.gamercollection.models.responses.GenreResponse
 import es.upsa.mimo.gamercollection.models.responses.PlatformResponse
@@ -32,31 +27,5 @@ class PopupFilterViewModel @Inject constructor(
         get() = genreRepository.getGenresDatabase()
     val platforms: List<PlatformResponse>
         get() = platformRepository.getPlatformsDatabase()
-    //endregion
-
-    //region Public methods
-    fun getRoundedSelectorButton(id: String, text: String, context: Context): Button {
-
-        val button = Button(
-            context,
-            null,
-            R.style.Widget_GamerCollection_Button_RoundedSelectorButton,
-            R.style.Widget_GamerCollection_Button_RoundedSelectorButton
-        )
-        button.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-        button.tag = id
-        button.text = text
-        button.setOnClickListener { selectButton(it) }
-        return button
-    }
-    //endregion
-
-    //region Private methods
-    private fun selectButton(button: View) {
-        button.isSelected = !button.isSelected
-    }
     //endregion
 }
