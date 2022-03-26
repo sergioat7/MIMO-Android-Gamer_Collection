6055cc1849c77446a892ab9340c4d81eeda867c3
Sergio Aragonés <sergioat7@gmail.com>
2022-03-26 21:11:43 +0100

    Removed format, genre, platform & state apis

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/responses/PlatformResponse.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/models/responses/PlatformResponse.kt
@@ -2,14 +2,11 @@ package es.upsa.mimo.gamercollection.models.responses
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 import es.upsa.mimo.gamercollection.base.BaseModel
 
 @Entity(tableName = "Platform")
 data class PlatformResponse(
     @PrimaryKey
-    @SerializedName("id")
     override val id: String,
-    @SerializedName("name")
     val name: String
 ) : BaseModel<String>
