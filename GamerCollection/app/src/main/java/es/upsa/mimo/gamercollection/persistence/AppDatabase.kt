301c3920669778bf39cf13e21e7a8cf308e82f1a
Sergio Aragonés <sergioat7@gmail.com>
2022-03-26 23:00:17 +0100

    Added database migration from 1 to 2

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/persistence/AppDatabase.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/persistence/AppDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import es.upsa.mimo.gamercollection.base.BaseModel
 import es.upsa.mimo.gamercollection.daos.GameDao
 import es.upsa.mimo.gamercollection.daos.SagaDao
@@ -17,7 +19,7 @@ import es.upsa.mimo.gamercollection.utils.Constants
     entities = [
         GameResponse::class,
         SagaResponse::class,
-        SongResponse::class], version = 1
+        SongResponse::class], version = 2
 )
 @TypeConverters(ListConverter::class, DateConverter::class)
 abstract class AppDatabase : RoomDatabase() {
@@ -29,6 +31,14 @@ abstract class AppDatabase : RoomDatabase() {
 
         //region Private properties
         private var instance: AppDatabase? = null
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DROP TABLE Format")
+                database.execSQL("DROP TABLE Genre")
+                database.execSQL("DROP TABLE Platform")
+                database.execSQL("DROP TABLE State")
+            }
+        }
         //endregion
 
         //region Public methods
@@ -37,11 +47,14 @@ abstract class AppDatabase : RoomDatabase() {
             if (instance == null) {
                 synchronized(AppDatabase::class) {
 
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        AppDatabase::class.java,
-                        Constants.DATABASE_NAME
-                    ).build()
+                    instance = Room
+                        .databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            Constants.DATABASE_NAME
+                        )
+                        .addMigrations(MIGRATION_1_2)
+                        .build()
                 }
             }
             return instance!!
