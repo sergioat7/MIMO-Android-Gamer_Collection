ccaa7daf8f00ad4e76322947cd41305daf28d551
Sergio Aragonés <sergioat7@gmail.com>
2022-04-06 23:35:36 +0200

    Removed edit text from saga item

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewholders/SagasViewHolder.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/viewholders/SagasViewHolder.kt
@@ -1,12 +1,9 @@
 package es.upsa.mimo.gamercollection.viewholders
 
-import android.text.InputType
 import androidx.recyclerview.widget.RecyclerView
-import es.upsa.mimo.gamercollection.R
 import es.upsa.mimo.gamercollection.adapters.OnItemClickListener
 import es.upsa.mimo.gamercollection.databinding.ItemSagaBinding
 import es.upsa.mimo.gamercollection.extensions.isDarkMode
-import es.upsa.mimo.gamercollection.extensions.setReadOnly
 import es.upsa.mimo.gamercollection.models.responses.SagaResponse
 
 class SagasViewHolder(val binding: ItemSagaBinding) :
@@ -14,23 +11,8 @@ class SagasViewHolder(val binding: ItemSagaBinding) :
 
     //region Public methods
     fun bind(saga: SagaResponse, onItemClickListener: OnItemClickListener) {
-
-        val gamesCount = saga.games.size
-        val title = if (gamesCount > 0) {
-            binding.root.context.resources.getQuantityString(
-                R.plurals.saga_title,
-                gamesCount,
-                saga.name,
-                gamesCount
-            )
-        } else {
-            saga.name
-        }
-
         with(binding) {
 
-            editTextName.setText(title)
-            editTextName.setReadOnly(true, InputType.TYPE_NULL, 0)
             this.saga = saga
             this.onItemClickListener = onItemClickListener
             this.isDarkMode = binding.root.context.isDarkMode()
