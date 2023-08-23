4d9b8cfe263b43aeb014dc4efca78a96385300d8
Sergio Aragonés <sergioat7@gmail.com>
2023-08-23 17:12:21 +0200

    Updated target version to Android 14

--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/ui/games/GamesFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/ui/games/GamesFragment.kt
@@ -588,31 +588,31 @@ class GamesFragment : BindingFragment<FragmentGamesBinding>(), OnItemClickListen
                 }
             })
         }
-        menuItem.setOnActionExpandListener(
-            object : MenuItem.OnActionExpandListener {
-                override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
-                    menu.let {
-//                        it.findItem(R.id.action_synchronize).isVisible = false
-                        it.findItem(R.id.action_filter).isVisible = false
-                        it.findItem(R.id.action_filter_fill).isVisible = false
-                        it.findItem(R.id.action_sort).isVisible = false
-                    }
-                    return true
-                }
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
 
-                override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
-                    menu.let {
-//                        it.findItem(R.id.action_synchronize).isVisible = true
-                        it.findItem(R.id.action_filter).isVisible = viewModel.filters.value == null
-                        it.findItem(R.id.action_filter_fill).isVisible =
-                            viewModel.filters.value != null
-                        it.findItem(R.id.action_sort).isVisible = true
-                    }
-                    return true
+                menu.let {
+//                    it.findItem(R.id.action_synchronize).isVisible = false
+                    it.findItem(R.id.action_filter).isVisible = false
+                    it.findItem(R.id.action_filter_fill).isVisible = false
+                    it.findItem(R.id.action_sort).isVisible = false
                 }
-
+                return true
             }
-        )
+
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+
+                menu.let {
+//                    it.findItem(R.id.action_synchronize).isVisible = true
+                    it.findItem(R.id.action_filter).isVisible = viewModel.filters.value == null
+                    it.findItem(R.id.action_filter_fill).isVisible =
+                        viewModel.filters.value != null
+                    it.findItem(R.id.action_sort).isVisible = true
+                }
+                return true
+            }
+
+        })
         setupSearchView(Constants.EMPTY_VALUE)
     }
     //endregion
--- GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/ui/sagas/SagasFragment.kt
+++ GamerCollection/app/src/main/java/es/upsa/mimo/gamercollection/ui/sagas/SagasFragment.kt
@@ -241,26 +241,26 @@ class SagasFragment : BindingFragment<FragmentSagasBinding>(), OnItemClickListen
                 }
             })
         }
-        menuItem.setOnActionExpandListener(
-            object : MenuItem.OnActionExpandListener {
-                override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
-                    menu.let {
-//                        it.findItem(R.id.action_synchronize).isVisible = false
-                        it.findItem(R.id.action_add).isVisible = false
-                    }
-                    return true
-                }
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
 
-                override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
-                    menu.let {
-//                        it.findItem(R.id.action_synchronize).isVisible = true
-                        it.findItem(R.id.action_add).isVisible = true
-                    }
-                    return true
+                menu.let {
+//                    it.findItem(R.id.action_synchronize).isVisible = false
+                    it.findItem(R.id.action_add).isVisible = false
                 }
-
+                return true
             }
-        )
+
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+
+                menu.let {
+//                    it.findItem(R.id.action_synchronize).isVisible = true
+                    it.findItem(R.id.action_add).isVisible = true
+                }
+                return true
+            }
+
+        })
         setupSearchView(Constants.EMPTY_VALUE)
     }
     //endregion
