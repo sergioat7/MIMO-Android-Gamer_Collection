8ed9fac110852d12dad72fb8f63940711844dd40
Sergio Aragonés <sergioat7@gmail.com>
2025-07-06 21:13:13 +0200

    Migrate dependencies to Version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,74 +84,64 @@ android {
 
 dependencies {
 
-    val lifecycleVersion = "2.9.1"
-    val retrofitVersion = "3.0.0"
-    val roomVersion = "2.7.2"
-    val navigationVersion = "2.9.1"
-    val hilt_version = "2.56.2"
-
     //Android
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.8")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.multidex:multidex:2.0.1")
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.fragment.ktx)
+    implementation(libs.material)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.multidex)
 
     //MVVM & LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
 
     //Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
 
     //Google Play Services
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
 
     //Gson
-    implementation("com.google.code.gson:gson:2.13.1")
+    implementation(libs.gson)
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0")
+    implementation(libs.bundles.retrofit)
 
     //Picasso
-    implementation("com.squareup.picasso:picasso:2.8")
+    implementation(libs.picasso)
 
     //Material RatingBar
-    implementation("me.zhanghai.android.materialratingbar:library:1.4.0")
+    implementation(libs.materialratingbar)
 
     //BlurImageView
-    implementation("com.github.jgabrielfreitas:BlurImageView:1.0.1")
+    implementation(libs.blurImageView)
 
     //Room
-    implementation("androidx.room:room-runtime:$roomVersion")
-    kapt("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation(libs.room.runtime)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
 
     //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation(libs.kotlinx.coroutines.android)
 
     //Security
-    implementation("androidx.security:security-crypto:1.0.0")
+    implementation(libs.security.crypto)
 
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-crashlytics-ktx")
-    implementation("com.google.firebase:firebase-config-ktx")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.bundles.firebase)
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:$hilt_version")
-    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
     //Test
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
